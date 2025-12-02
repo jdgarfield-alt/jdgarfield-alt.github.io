f29e934b411b6157c07467ce2b3142cb191119d4
Di <jind@uleth.ca>
2025-12-02 14:00:20 -0700

    feat: implement initial portfolio website with core pages, styling, and interactive elements.

---
+++ script.js
@@ -0,0 +1,57 @@
+/* Toggle Icon Navbar */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/* Sticky Navbar */
+window.onscroll = () => {
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* Remove toggle icon and navbar when click navbar link (scroll) */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/* Scroll Reveal */
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/* Typed JS */
+const typed = new Typed('.multiple-text', {
+    strings: ['Data Analyst', 'Storyteller', 'Problem Solver'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+/* Custom Cursor */
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, { duration: 500, fill: "forwards" });
+});
